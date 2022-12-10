c25f1f4d761d0634b70f015e1cf291a3f04be122
Sheice <75450007+Sheice@users.noreply.github.com>
2022-12-10 18:10:56 -0300

    crud of laptop

--- src/main/java/com/laptop/laptop/controllers/LaptopRepository.java
+++ src/main/java/com/laptop/laptop/controllers/LaptopRepository.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -22,10 +23,58 @@ public class LaptopRepository {
         return (List<Laptop>)laptopRepository.findAll();
     }
 
-    @PostMapping("/create/laptop")
-    public Laptop saveLaptop(@RequestBody Laptop laptop) {
-        System.out.println(laptop.getMarca());
-        return laptopRepository.save(laptop);
+    @GetMapping("/laptop/{id}")
+    public ResponseEntity<Laptop> traerLaptopPorId(@PathVariable Long id){
+        Optional<Laptop> laptop = laptopRepository.findById(id);
+
+        if(laptop.isPresent()){
+            return ResponseEntity.ok(laptop.get());
+        }
+        return ResponseEntity.notFound().build();
     }
+
+    @PostMapping("/create/laptop")
+    public ResponseEntity<Laptop> saveLaptop(@RequestBody Laptop laptop) {
+        if (laptop.getId() != null){
+            return  ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(laptopRepository.save(laptop));
+    }
+
+    @PutMapping("/update/laptop/{id}")
+    public ResponseEntity<Laptop> actualizarLaptop(@RequestBody Laptop laptop, @PathVariable Long id){
+
+        if (laptop.getId() == null){
+            return  ResponseEntity.badRequest().build();
+        }
+
+        if(!laptopRepository.existsById(id)){
+            return  ResponseEntity.notFound().build();
+        }
+
+        Laptop result = laptopRepository.save(laptop);
+
+        return ResponseEntity.ok(result);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Laptop> borrarLaptop(@PathVariable Long id){
+
+        if(!laptopRepository.existsById(id)){
+            return  ResponseEntity.notFound().build();
+        }
+        laptopRepository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/delete/all")
+    public ResponseEntity<Laptop> borrarTodasLasLaptops(){
+        laptopRepository.deleteAll();
+
+        return ResponseEntity.noContent().build();
+    }
+
+
 
 }
